ac71272ea22b361ce874559b0bd79593327e05b1
Sam-Serpoosh <sam.serpoosh@gmail.com>
2013-11-18 00:37:16 -0500

    Extract dependencies in class definition code - other classes

---
+++ dependency_extractor.rb
@@ -0,0 +1,41 @@
+class DependencyExtractor
+  attr_reader :dependencies
+
+  def initialize(klass, code_lines)
+    @dependencies = []
+    @code_lines = code_lines
+    @klass = klass
+  end
+
+  def extract
+    @code_lines.each do |line|
+      add_dependency_in_line(line)
+    end
+  end
+
+  private
+
+  def add_dependency_in_line(line)
+    tokens = tokenize_line(line)
+    possible_class_names = camel_case_tokens(tokens)
+    possible_class_names.each do |name|
+      name_sym = name.to_sym
+      @dependencies << name_sym if name_sym != @klass
+    end
+  end
+
+  def tokenize_line(line)
+    tokens = line.split(" ").select do |token|
+      token.strip != ""
+    end.map(&:strip)
+    tokens.map do |token|
+      token.split(/\./)
+    end.flatten
+  end
+
+  def camel_case_tokens(tokens)
+    tokens.select do |token|
+      ("A".."Z").include?(token[0])
+    end
+  end
+end
---
+++ dependency_extractor_spec.rb
@@ -0,0 +1,19 @@
+require_relative "./dependency_extractor"
+
+describe DependencyExtractor do
+  it "detects usage of another class" do
+    code_lines = ["class Foo", "def foo", "bar = Bar.new", 
+                  "bar.some_method", "end"]
+    dep_extractor = DependencyExtractor.new(:Foo, code_lines)
+    dep_extractor.extract
+    dep_extractor.dependencies.should include(:Bar)
+  end
+
+  it "detects dependencies when multiple collaborators exist" do
+    code_lines = ["class Foo", "Bar.some_method", "def foo", 
+                  "Baz.new", "baz.func", "end"]
+    dep_extractor = DependencyExtractor.new(:Foo, code_lines)
+    dep_extractor.extract
+    dep_extractor.dependencies.should == [:Bar, :Baz]
+  end
+end
